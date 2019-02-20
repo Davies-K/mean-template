18afcb4175eab82fde0763eb90463e193c51b2d8
Vasanth Kesavan <vk432@njit.edu>
2019-02-19 19:19:18 -0500

    add mongoose and database connection

--- app.ts
+++ app.ts
@@ -1,14 +1,22 @@
+import express = require("express");
+import mongoose = require("mongoose");
 
-// lib/app.ts
-import express = require('express');
-
-// Create a new express application instance
 const app: express.Application = express();
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-});
+// Everything to start the application
+async function connect() {
+    try {
+        await mongoose.connect("mongodb://localhost/test", {useNewUrlParser: true});
+        app.listen(3000, function () {
+            console.log('Example app listening on port 3000!');
+        });
+    } catch(e) {
+        console.log(e);
+    }
+};
+
+connect();
