fa9c8e614bbfc7b26c12a1c5b8e83b3b63d94ff9
Vasanth Kesavan <vk432@njit.edu>
2019-02-24 16:26:58 -0500

    handle access denied error on page reload

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ app.use(helmet());
 // SERVE THE APP
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './build/index.html')));
 
+// OTHERS: NG HANDLED
+app.all('/*', (req, res) => res.sendFile(path.join(__dirname, './build/index.html')));
+
 // APP ROUTES
 app.use("/api/users", require("./api/users"));
 app.use("/api/auth", require("./api/authenticate"));
@@ -27,6 +30,7 @@ app.use(generalUtils.checkJwt);
 app.use(generalUtils.attachUser);
 
 // AUTHENTICATED ROUTES
+
 
 
 async function connect() {
