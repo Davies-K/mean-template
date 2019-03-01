5ec5dc280d6e910afdc23d7128f026e58807663f
Vasanth Kesavan <vasanthkesavan09@gmail.com>
2019-02-28 19:28:57 -0500

    change button color based on current route

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {Observable} from "rxjs";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Observable, Subscription} from "rxjs";
 import {CoreService} from "./core/core.service";
-import {Router} from "@angular/router";
+import {NavigationEnd, Router} from "@angular/router";
 
 interface Nav {
   link: string,
@@ -14,13 +14,33 @@ interface Nav {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   isLoggedIn$: Observable<boolean>;
+  isLoginActive: boolean = false;
+  isSignupActive: boolean = false;
+  routerEventsSub: Subscription;
 
   constructor(private coreService: CoreService, private router: Router) {}
 
   ngOnInit(): void {
+
+    this.routerEventsSub = this.router.events.subscribe((url: any) => {
+      if(url instanceof NavigationEnd) {
+        if(this.router.url === '/login') {
+          this.isSignupActive = false;
+          this.isLoginActive = true;
+        } else if (this.router.url === '/sign-up') {
+          this.isSignupActive = true;
+          this.isLoginActive = false;
+        }
+      }
+    });
+
     this.isLoggedIn$ = this.coreService.getAuthStatus();
+  }
+
+  ngOnDestroy() {
+    this.routerEventsSub.unsubscribe();
   }
 
   async onLogout() {
